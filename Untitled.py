c504b81627f41573b4564fbfff4881891baac678
Jonathan Pharies <pharies@Jonathans-MacBook-Pro.local>
2019-09-28 17:58:49 -0400

    done

--- Untitled.py
+++ Untitled.py
@@ -3,8 +3,10 @@
 # Jack Pharies and Rob Ranseen
 
 
-
-
+# opens file called data.txt and creates a global list of points
+# first number in list is the amount of variables
+# in data.txt, x cords are on the left and y cords are on the right
+def getData()
 
 
 
