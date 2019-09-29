9683e1ab53d4e3aa5257963631afc2003b6699a0
Jonathan Pharies <pharies@Jonathans-MacBook-Pro.local>
2019-09-29 13:43:11 -0400

    finished find line a,b,c calculated

--- convex.py
+++ convex.py
@@ -9,6 +9,9 @@ import random
 # global vars
 points = []
 count = 0
+found = False
+convexList = []
+
 
 # opens file called data.txt and creates a global list of points
 # first number in list is the amount of variables
@@ -42,7 +45,7 @@ def getData():
 # creates a list of psuedo-randomly generated pojnts of length n
 # points that are generated are between the range of 0 and 100
 #
-# inputs : n = length of list.
+# param : n = length of list.
 def createData(n):
     
     gate = n
@@ -70,7 +73,7 @@ def createData(n):
 def getConvexHull():
     firstItem = points[0]
     xLow = firstItem[0]
-    convexList = []
+    
 
     # since we know the an extreme must be the lowest x value, we start by finding the smallest x value
     for item in points:
@@ -79,26 +82,63 @@ def getConvexHull():
 
     
     convexList.append(xLow)
-    found = False
-    indexCon = 0
-    indexCheck = 0
-    currentPoint = convexList[indexCon]
-    currentCheck = points[indexCheck]
+    
+  
     while found == False:
+  
         for point in convexList:
-            print(convexList)
-            for testPoint in points:
+            nextPoint = False
+            pointsCount = points.length()
+            while nextPoint == False and pointsCount >= 0:
+                testPoint = points[pointsCount]
+                if point[0] =! testPoint[0] and point[1] =! testPoint[1]:
+                    nextPoint = checkLineSegment(point, testPoint)
+            if nextPoint == True:
+                convexList.append(testPoint)
                 
+    return convexList
+                    
 
+# checks if all the points of points lie on one side of the line or not
+#
+# param : index1, index2 = two distinct points from the list of points
+#
+# output : True if all the points lie on one side
+#          False if the points do not all lie on same side
+def checkLineSegment(index1, index2):
+    a, b, c = findLine()
 
-
-                #if point[0] =! testPoint[0] and point[1] =! testPoint[1] 
+    for items in points:
+        
     
 
 
 
 
 
+# calculates the values a, b, c by from the parameters by which the formula ax+by=c is made
+#
+# param : index1, index2 = two distinct points from the list of points
+#
+# output : a,b,c = the three values that are calculated 
+def findLine(index1, index2):
+
+    a = index2[1] - index1[1]
+
+    b = index1[0] - index2[0]
+
+    c = (index1[0] * index2[1]) - (index2[0] * index1[1])
+
+    return a, b, c
+    
+
+
+
+
+
+
+
+    
 
 # main method, calls the other methods
 def main():
